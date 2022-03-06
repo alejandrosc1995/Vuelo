24610b9bdbedf0200e5bb477be4caaebbeb38eb0
alejandrosc1995 <alejosc_@hotmail.com>
2022-03-06 18:30:47 -0500

    Modificacion pasajeros

--- src/main/java/seguridad/airlines/Pasajero.java
+++ src/main/java/seguridad/airlines/Pasajero.java
@@ -7,6 +7,8 @@ public class Pasajero {
     private final String identificador;
     private final String name;
     private final String codigoCiudad;
+    private Vuelo vuelo;
+    
     
     public Pasajero(final String identificador, final String name, final String codigoCiudad){
         if(!Arrays.asList(Locale.getISOCountries()).contains(codigoCiudad)){
@@ -28,6 +30,31 @@ public class Pasajero {
     public String getCodigoCiudad() {
         return codigoCiudad;
     }
+
+    public Vuelo getVuelo() {
+        return vuelo;
+    }
+
+    public void setVuelo(Vuelo vuelo) {
+        this.vuelo = vuelo;
+    }
+    
+    public void joinFlight(Vuelo vuelo){
+        Vuelo vueloPrevio = this.vuelo;
+        if(null !=vueloPrevio){
+            if(!vueloPrevio.removerPasajero(this)){
+                throw new RuntimeException("No puede remover pasajeros");
+            }
+        }
+        setVuelo(vuelo);
+        if(null != vuelo){
+            if(!vuelo.addPasajeros(this)){
+                throw new RuntimeException("No puede agregar pasajeros");
+            }
+        }
+    }
+    
+    
 
     @Override
     public String toString() {
